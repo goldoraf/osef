9384127c4aa02191855fac66ccbf7b08f5e88f82
Raphaël Rougeron <goldoraf@gmail.com>
2013-09-26 17:01:52 +0200

    Greatly enhanced the EventBus, it now supports namespacing and wildcards

--- src/wires/event_bus.js
+++ src/wires/event_bus.js
@@ -1,22 +1,125 @@
-function CustomEvent (event, params) {
-    params = params || { bubbles: false, cancelable: false, detail: undefined };
-    var evt = document.createEvent( 'CustomEvent' );
-    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-    return evt;
-};
-CustomEvent.prototype = window.CustomEvent.prototype;
-window.CustomEvent = CustomEvent;
 
 var EventBus = {
-    subscribe: function(name, callback) {
-        document.addEventListener(name, function(e) {
-            callback(e.detail);
-        });
+    messages: {},
+    wildcards: [],
+    uuid: 0,
+
+    subscribe: function(message, callback, context) {
+        if (!this.messages.hasOwnProperty(message)) {
+            this.messages[message] = [];
+        }
+        var token = String(++this.uuid);
+        this.messages[message].push({ token: token, callback: callback, context: context });
+        if (this.containsWildcards(message)) {
+            this.addWildcard(message);
+        }
+        return token;
     },
 
-    publish: function(name, data) {
-        var customEvent = new CustomEvent(name, { detail: data});
-        document.dispatchEvent(customEvent);
+    publish: function(message, data) {
+        var matchedWildcards = this.matchWildcards(message);
+        matchedWildcards.forEach(function(wildcardedMessage) {
+            this.deliverMessage(message, wildcardedMessage, data);
+        }, this);
+
+        if (!this.hasSubscribers(message) && matchedWildcards.length === 0) {
+            return false;
+        }
+        this.deliverNamespaced(message, data);
+        return true;
+    },
+
+    hasSubscribers: function(message) {
+        var topic = String(message),
+            found = this.messages.hasOwnProperty(topic),
+            position = topic.lastIndexOf('.');
+
+        while (!found && position !== -1) {
+            topic = topic.substr(0, position);
+            position = topic.lastIndexOf('.');
+            found = this.messages.hasOwnProperty(topic);
+        }
+
+        return found && this.messages[topic].length > 0;
+    },
+
+    deliverNamespaced: function(message, data) {
+        var topic = String(message),
+            position = topic.lastIndexOf('.');
+
+            this.deliverMessage(message, message, data);
+
+            while (position !== -1) {
+                topic = topic.substr(0, position);
+                position = topic.lastIndexOf('.');
+                this.deliverMessage(message, topic, data);
+            }
+    },
+
+    deliverMessage: function(originalMessage, matchedMessage, data) {
+        if (!this.messages.hasOwnProperty(matchedMessage)) {
+            return;
+        }
+
+        var i, subscribers = this.messages[matchedMessage];
+
+        // do not cache the length of the subscribers array, 
+        // as it might change if there are unsubscribtions during delivery of a message
+        for (i = 0; i < subscribers.length; i++) {
+            this.callSubscriber(subscribers[i], originalMessage, data);
+        }
+    },
+
+    callSubscriber: function(subscriber, message, data) {
+        if (subscriber.context !== undefined) {
+            subscriber.callback.call(subscriber.context, message, data);
+        } else {
+            subscriber.callback(message, data);
+        }
+    },
+
+    containsWildcards: function(message) {
+        return message.indexOf('*') !== -1;
+    },
+
+    addWildcard: function(message) {
+        this.wildcards.push({ regex: message.replace(/(\*)/, '([\\w-]+)'), message: message });
+    },
+
+    matchWildcards: function(message) {
+        var messages = [];
+        this.wildcards.forEach(function(wildcard) {
+            if (message.match(wildcard.regex)) {
+                messages.push(wildcard.message);
+            }
+        });
+        return messages;
+    },
+
+    unsubscribe: function(tokenOrCallback) {
+        var isToken = typeof tokenOrCallback === 'string',
+            property = isToken ? 'token' : 'callback',
+            result = false,
+            m, i;
+
+        for (var m in this.messages) {
+            if (this.messages.hasOwnProperty(m)) {
+                for (i = this.messages[m].length - 1 ; i >= 0; i--) {
+                    if (this.messages[m][i][property] === tokenOrCallback) {
+                        this.messages[m].splice(i, 1);
+                        if (isToken) {
+                            return true;
+                        }
+                        result = true;
+                    }
+                }
+            }
+        }
+        return result;
+    },
+
+    unsuscribeAll: function() {
+        this.messages = {};
     }
 };
 
--- test/js/spec/event_bus.js
+++ test/js/spec/event_bus.js
@@ -1,11 +1,125 @@
-var EventBus = Osef.wires.EventBus;
+var assert = chai.assert,
+    event = { data: 42 },
+    EventBus = Osef.wires.EventBus;
 
-describe('EventBus tests', function() {
-    it('should work ;)', function(done) {
-        EventBus.subscribe('foo', function(e) {
-            chai.assert.equal(e.data, 42);
+describe('EventBus', function() {
+    
+    beforeEach(function() {
+        EventBus.unsuscribeAll();
+    });
+
+    describe('subscribe', function() {
+        it('should register a callback function for a given event name', function(done) {
+            var callback = sinon.spy();
+            EventBus.subscribe('foo', callback);
+            EventBus.publish('foo', event);
+            assert(callback.called);
             done();
         });
-        EventBus.publish('foo', { data: 42 });
+
+        it('should allow to subscribe a given callback function to many events', function(done) {
+            var callback = sinon.spy();
+            EventBus.subscribe('foo', callback);
+            EventBus.subscribe('bar', callback);
+            EventBus.publish('foo', event);
+            assert(callback.calledOnce);
+            EventBus.publish('bar', event);
+            assert(callback.calledTwice);
+            done();
+        });
+
+        it('should return a token for later unsuscribe', function() {
+            assert.ok(typeof EventBus.subscribe('foo', function() {}) == 'string');
+        });
+
+        it('should support hierarchical addressing', function(done) {
+            var callback = sinon.spy();
+            EventBus.subscribe('foo', callback);
+            EventBus.publish('foo.bar', event);
+            EventBus.publish('foo.baz', event);
+            assert(callback.calledTwice);
+            done();
+        });
+
+        it('should accept a context for the callback function', function(done) {
+            var dummy = {
+                foo: 'bar',
+                created: function(msg, data) {
+                    assert.equal('bar', this.foo);
+                }
+            };
+            EventBus.subscribe('foo', dummy.created, dummy);
+            EventBus.publish('foo', event);
+            done();
+        });
+
+        it('should accept wildcards in event names', function(done) {
+            var handler = {
+                handle: function(msg, data) {}
+            };
+            var spy = sinon.spy(handler, 'handle');
+            EventBus.subscribe('foo.test-*', handler.handle);
+            EventBus.publish('foo.test-123.dummyCreated', event);
+            assert(spy.calledOnce);
+            assert(spy.calledWith('foo.test-123.dummyCreated'));
+            done();
+        });
+    });
+
+    describe('publish', function() {
+        it('should pass the event name and the event\'s data to the subscribed handler', function(done) {
+            var callback = sinon.spy();
+            EventBus.subscribe('bar', callback);
+            EventBus.publish('bar', event);
+            assert(callback.calledWith('bar', event));
+            done();
+        });
+
+        it('should return false if there is no subscribers to the message', function(done) {
+            assert.notOk(EventBus.publish('foo', event));
+            EventBus.subscribe('foo', function() {});
+            assert.ok(EventBus.publish('foo', event));
+            done();
+        });
+    });
+
+    describe('unsuscribe', function() {
+        it('should accept a token and cancel a specific subscription', function(done) {
+            var callback = sinon.spy(),
+                token1 = EventBus.subscribe('foo', callback),
+                token2 = EventBus.subscribe('bar', callback);
+            EventBus.unsubscribe(token1);
+            EventBus.publish('foo', event);
+            assert(!callback.called);
+            EventBus.publish('bar', event);
+            assert(callback.called);
+            done();
+        });
+
+        it('should accept a callback and cancel all its subscriptions', function(done) {
+            var callback = sinon.spy();
+            EventBus.subscribe('foo', callback);
+            EventBus.subscribe('bar', callback);
+            EventBus.unsubscribe(callback);
+            EventBus.publish('foo', event);
+            EventBus.publish('bar', event);
+            assert(!callback.called);
+            done();
+        });
+    });
+
+    describe('unsuscribeAll', function() {
+        it('should unsuscribe everything', function(done) {
+            var cb1 = sinon.spy(), cb2 = sinon.spy();
+            EventBus.subscribe('foo', cb1);
+            EventBus.subscribe('foo', cb2);
+            EventBus.subscribe('bar', cb1);
+            EventBus.unsuscribeAll();
+            EventBus.publish('foo', event);
+            EventBus.publish('bar', event);
+            assert(!cb1.called);
+            assert(!cb2.called);
+            done();
+        });
     });
 });
